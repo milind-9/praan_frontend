ed02150cf02cb5b5fb53f0f7c09e28a11041a6d8
milind-9 <143262139+milind-9@users.noreply.github.com>
2023-09-07 14:54:28 +0530

    Update ComparisonChart.js

--- src/ComparisonChart.js
+++ src/ComparisonChart.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const ComparisonChart = ({ data }) => {
-  console.log(data,'==============');
+  console.log(data, '==============');
 
-  // Function to map data and generate lines for a specific PM value
+  // Function to generate lines for a specific PM value
   const renderLines = (dataKey, name, strokeColor) => (
     <Line
       type="monotone"
@@ -14,8 +14,11 @@ const ComparisonChart = ({ data }) => {
     />
   );
 
+  // Generate a random color for each line
+  const getRandomColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
   return (
-    <LineChart width={800} height={400} data={data}>
+    <LineChart width={800} height={400} data={data.locations[0].data}>
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="time" />
       <YAxis />
@@ -23,24 +26,15 @@ const ComparisonChart = ({ data }) => {
       <Legend />
 
       {/* Render lines for PM1 */}
-      {data.locations.map((location, index) => (
-        renderLines(`locations[${index}].data[].pm1`, `${location.name} PM 1.0`, getRandomColor())
-      ))}
+      {renderLines('pm1', `${data.locations[0].name} PM 1.0`, getRandomColor())}
 
       {/* Render lines for PM2.5 */}
-      {data.locations.map((location, index) => (
-        renderLines(`locations[${index}].data[].pm25`, `${location.name} PM 2.5`, getRandomColor())
-      ))}
+      {renderLines('pm25', `${data.locations[0].name} PM 2.5`, getRandomColor())}
 
       {/* Render lines for PM10 */}
-      {data.locations.map((location, index) => (
-        renderLines(`locations[${index}].data[].pm10`, `${location.name} PM 10`, getRandomColor())
-      ))}
+      {renderLines('pm10', `${data.locations[0].name} PM 10`, getRandomColor())}
     </LineChart>
   );
 };
-
-// Generate a random color for each line
-const getRandomColor = () => `#${Math.floor(Math.random()*16777215).toString(16)}`;
 
 export default ComparisonChart;
